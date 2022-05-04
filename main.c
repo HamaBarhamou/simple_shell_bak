20976986624cb437cbf114b0546cae7075a7cc55
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-04 15:40:40 +0100

    rempleacement de la fonction printf par _printf

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "main.h"
 
 #define Buff_Size 1024
@@ -13,14 +14,15 @@
 int main(void)
 {
 	ssize_t r;
-	char buff[Buff_Size + 1];
+	char *buff;
 
 	while (1)
 	{
-		printf("($) ");
-		r = read(1, &buff, Buff_Size);
+		_puts("($) ");
+		buff = malloc(sizeof(char) * Buff_Size + 1);
+		r = read(1, buff, Buff_Size);
 		buff[_strlen(buff) + 1] = '\0';
-		/*printf("\n buff= %s",buff);*/
+		/*_puts(buff);*/
 		UNUSED(r);
 		UNUSED(buff);
 	}
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 
+int _putchar(char c);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* MAIN_H */
