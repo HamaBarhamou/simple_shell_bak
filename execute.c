4c9fbcce43367119b1d32b8b2a01fd650aa1a1bd
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-04 21:44:48 +0100

    ajout exit et preparation de l'imolementation de la fonction executecmd

---
+++ execute.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _executecmd - a function ...
+ * @cmd: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+int	_executecmd(char *cmd)
+{
+	_puts("Reste a implementer l'execution de la commande dans execute.c \n");
+	UNUSED(cmd);
+	return (1);
+}
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 #define Buff_Size 1024
-#define UNUSED(x) (void)(x)
 
 /**
  * main - check the code
@@ -22,9 +22,15 @@ int main(void)
 		buff = malloc(sizeof(char) * Buff_Size + 1);
 		r = read(1, buff, Buff_Size);
 		buff[_strlen(buff) - 1] = '\0';
+
+		if (_strcmp("exit", buff) == 0)
+		{
+			free(buff);
+			exit(0);
+		}
+
+		_executecmd(buff);
 		/*_puts(buff);*/
-		UNUSED(r);
-		UNUSED(buff);
 	}
 	return (0);
 }
